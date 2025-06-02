0280908fc8ca19b6815a60ce4c60412a08a0a5f2
mac <mac@mactekiMacBook-Pro.local>
2025-06-02 12:06:05 +0300

    added src and test files

--- test/unitest.py
+++ test/unitest.py
@@ -1,11 +1,8 @@
 import unittest
-from lab1 import is_monotone1
+from src.lab1 import is_monotone1
 class MonotoneArray(unittest.TestCase):
     
     def test_increasing(self):
         self.assertTrue(is_monotone1([1, 2, 3, 4, 5]))
-        
-    # def test_decreasing(self):
-    #     self.assertTrue(is_monotone2([5, 4, 3, 2, 1]))
         
 unittest.main()
